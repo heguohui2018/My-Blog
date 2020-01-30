8dfb2282acd212181fe22e2079192c5c698d6edd
heguohui2018 <heguohui2017@outlook.com>
2020-01-30 21:55:53 +0800

    add php变量

---
+++ web/Semantic学习/php/php变量.php
@@ -0,0 +1,29 @@
+<?php
+	/* echo "hello world"; */
+	/* 变量命名规则： */
+	/* 前缀为：$ */
+	/* 变量名以字母，下划线开头，不能使用数字开头 */
+	/* 采用驼峰式命名 */
+	/* $output = "hello world"; */
+	/* echo $output; */
+
+	/* 数据类型 */
+	/* integer 数字类型 */
+	/* string 字符串类型 */
+	/* float 浮点类型 */
+	/* boolean 布尔类型 */
+	/* array 数组类型 */
+	/* object 对象类型 */
+	/* null 空对象 */
+	/* resource */ 
+
+	$number = 10;
+	echo "<p>$number</p>";
+
+	$float = 5.5;
+	echo $float;
+
+	$bool = true;
+	echo "<p>$bool</p>";
+?>
+
